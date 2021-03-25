76e9997885bdafac39c8e9515ae68f3c23293086
Bruno Laviso <bruno.laviso@hotmail.com>
2021-03-24 21:08:08 -0300

    refactor: delete unusless file and rename js to tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>BATcoder | Início</title>
+      </Head>
+      <h1>Hello BATcoder</h1>
+    </>
+  )
+}
