7cdb40ab542e37951b10df8e3799ec89a21e979d
Bruno Laviso <bruno.laviso@hotmail.com>
2021-03-24 22:30:11 -0300

    feat: add CTA in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import Head from 'next/head'
+import { CallTheAction } from '../components/CallTheAction'
 
 export default function Home() {
   return (
     <>
       <Head>
+        <meta name="description" content="Desafios de programação para desenvolvedores evoluirem suas habilidades."/>
         <title>BATcoder | Início</title>
       </Head>
-      <h1>Hello BATcoder</h1>
+      <CallTheAction 
+        title="Desafios de programação para desenvolvedores evoluirem suas habilidades."
+        subtitle="Confira os desafios disponíveis na nossa plataforma."
+        textButton="Bora lá!"
+        imgPath="./images/programmer.svg"
+      />
     </>
   )
 }
