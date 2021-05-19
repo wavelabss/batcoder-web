e2b43e0d0aac9c5696ce188a0f3e7c5c25653326
Bruno Laviso <bruno.laviso@hotmail.com>
2021-05-18 23:06:04 -0300

    bugfix: path from logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,8 @@ import styles from './styles.module.scss'
 export function Header() {
   return (
     <header className={styles.headerContainer}>
-    <div className={styles.headerContent}>
-        <img src="./images/logo.svg" alt="BATcoder" title="BATcoder" />
+      <div className={styles.headerContent}>
+        <img src="./images/logo-white-green.svg" alt="BATcoder" title="BATcoder" />
         <nav>
           <a className={styles.active} href="#">Desafios</a>
           <a href="#">Comunidade</a>
