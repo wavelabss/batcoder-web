347a307bcea99cc194ba885be9601ad21b159ba3
Bruno Laviso <bruno.laviso@hotmail.com>
2021-05-18 23:06:29 -0300

    feat: create footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import styles from './styles.module.scss'
+
+export function Footer() {
+  return (
+    <footer className={styles.FooterSection}>
+      <main>
+        <div className={styles.FooterIcons}>
+          <a href=""><img src="./images/icons/github.svg" alt=""/></a>
+          <a href=""><img src="./images/icons/instagram.svg" alt=""/></a>
+          <a href=""><img src="./images/icons/linkedin.svg" alt=""/></a>
+        </div>
+        <div className={styles.FooterLogo}>
+          <img src="./images/logo-black-white.svg" alt="BATcoder" title="BATcoder" />
+        </div>
+      </main>
+    </footer>
+  )
+}
