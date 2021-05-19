9eedc5a349693ffd36d89f722febc34be7f0ad94
Bruno Laviso <bruno.laviso@hotmail.com>
2021-05-18 23:07:39 -0300

    feat: create list and details page

---
+++ src/pages/challenges/details.tsx
@@ -0,0 +1,5 @@
+export default function DetailsChallenge() {
+  return (
+    <h1>DetailsChallenge</h1>
+  )
+}
---
+++ src/pages/challenges/list.tsx
@@ -0,0 +1,16 @@
+import { CardChallenges } from '../../components/CardChallenges'
+
+export default function ListChallenges() {
+  return (
+    <CardChallenges>
+      <img src="./images/creaters/ana.png" alt=""/>
+      <strong>Ana Laura</strong>
+      <p>
+        Estudante de ciencia da computação, apaixonada por fazer conexões e inspirar pessoas.<br />
+        Manja dos designs e adora organizar eventos de tecnolgia.<br />
+        Acredita que o segredo para mudar o mundo é ocupar lugares importatantes.
+      </p>
+      <button>Detalhes</button>
+    </CardChallenges>
+  )
+}
