568c1c9071e9edcfcd66ab6484ce1a809ee40807
Bruno Laviso <bruno.laviso@hotmail.com>
2021-05-20 22:20:04 -0300

    feat: list challenge with data from api

--- src/pages/challenges/list.tsx
+++ src/pages/challenges/list.tsx
@@ -1,16 +1,31 @@
+import { useEffect, useState } from 'react'
 import { CardChallenges } from '../../components/CardChallenges'
+import { api } from '../../service/api'
+import { IChallenge } from '../../shared/interfaces/challenge'
+
+import styles from '../styles/ListChallenges.module.scss'
 
 export default function ListChallenges() {
+  const [challenges, setChallenges] = useState<IChallenge[]>([])
+
+  useEffect(() => {
+    api.get<IChallenge[]>('challenges').then(response => {
+      setChallenges(response.data)
+    })
+  }, [])
+
   return (
-    <CardChallenges>
-      <img src="https://i.imgur.com/rfD5Y3Y.png" alt=""/>
-      <strong>Ana Laura</strong>
-      <p>
-        Estudante de ciencia da computação, apaixonada por fazer conexões e inspirar pessoas.<br />
-        Manja dos designs e adora organizar eventos de tecnolgia.<br />
-        Acredita que o segredo para mudar o mundo é ocupar lugares importatantes.
-      </p>
-      <button>Detalhes</button>
-    </CardChallenges>
+    <main className={styles.ListChallengesContainer}>
+      {
+        challenges.map(challenge => (
+      <CardChallenges marginBottom={32} key={challenge.id}>
+        <img src={challenge.cover} alt=""/>
+        <strong>{challenge.title}</strong>
+        <p>{challenge.description}</p>
+        <button>Detalhes</button>
+      </CardChallenges>
+        ))
+      }
+    </main>
   )
 }
