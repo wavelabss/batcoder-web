edf2f0fee49ce7811d42fb62eedfc61decf60098
Bruno Laviso <bruno.laviso@hotmail.com>
2021-05-20 22:19:42 -0300

    feat: add datas of challenge to details page

--- src/pages/challenges/details.tsx
+++ src/pages/challenges/details.tsx
@@ -1,5 +1,26 @@
-export default function DetailsChallenge() {
+import { useEffect, useState } from 'react'
+import { api } from '../../service/api'
+import { IChallenge } from '../../shared/interfaces/challenge'
+
+import styles from '../styles/DetailsChallenge.module.scss'
+
+export default function DetailsChallenge(id: number) {
+  const [challenge, setChallenge] = useState<IChallenge>()
+
+  useEffect(() => {
+    api.get<IChallenge>('challenges/2').then(response => {
+      console.log(response)
+      setChallenge(response.data)
+    })
+  }, [])
   return (
-    <h1>DetailsChallenge</h1>
+    <main className={styles.DetailsChallengeContainer}>
+      <div className={styles.DetailsChallengeCard}>
+        <strong>{challenge?.title}</strong>
+        <img src={challenge?.cover} alt="" />
+        <p>{challenge?.description}</p>
+        <button>Codar</button>
+      </div>
+    </main>
   )
 }
