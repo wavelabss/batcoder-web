31997b743d4079ff2871af24e1a69b604c76b6fa
Bruno Laviso <bruno.laviso@hotmail.com>
2021-05-24 11:28:27 -0300

    feat: add Head tag and remove console.log

--- src/pages/challenges/[slug].tsx
+++ src/pages/challenges/[slug].tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 import { api } from '../../service/api'
 import { IChallenge } from '../../shared/interfaces/challenge'
 
@@ -13,18 +14,23 @@ export default function DetailsChallenge(id: number) {
 
   useEffect(() => {
     api.get<IChallenge>(`challenges?slug=${slug}`).then(response => {
-      console.log(response)
       setChallenge(response.data[0])
     })
   }, [slug])
   return (
-    <main className={styles.DetailsChallengeContainer}>
-      <div className={styles.DetailsChallengeCard}>
-        <strong>{challenge?.title}</strong>
-        <img src={challenge?.cover} alt="" />
-        <p>{challenge?.description}</p>
-        <button>Codar</button>
-      </div>
-    </main>
+    <>
+      <Head>
+        <meta name="description" content="Desafios de programação para desenvolvedores evoluirem suas habilidades."/>
+        <title>BATcoder | {challenge?.title} </title>
+      </Head>
+      <main className={styles.DetailsChallengeContainer}>
+        <div className={styles.DetailsChallengeCard}>
+          <strong>{challenge?.title}</strong>
+          <img src={challenge?.cover} alt="" />
+          <p>{challenge?.description}</p>
+          <button>Codar</button>
+        </div>
+      </main>
+    </>
   )
 }
