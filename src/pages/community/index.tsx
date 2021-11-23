be61df6d4bb57df4e51f176bf14a73bf3c69e3ac
Bruno Laviso <bruno.laviso@uppercase.com.br>
2021-11-23 15:31:01 -0300

    styles: :art: adjustes in community page

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { CallTheAction } from '../../components/CallTheAction'
 
+import styles from './styles.module.scss'
+
 export default function Community() {
   return (
     <>
@@ -8,12 +10,14 @@ export default function Community() {
         <meta name="description" content="Comunidade de programadores que amam tecnologia" />
         <title>BATcoder | Comunidade</title>
       </Head>
-      <CallTheAction>
-        <h1>Participe da nossa comunidade no discord. <br />É rápido e indolor.</h1>
-        <h2>Todos os integrantes são muito simpáticos. Manda um salve no chat-geral.</h2>
-        <a href="https://discord.gg/AHbF8BK" target="_blank" rel="noreferrer">Acessar!</a>
-        <img src="./images/community.svg" alt="Pessoas conversando digitalmente"/>
-      </CallTheAction>
+      <main className={styles.CommunityContent}>
+        <CallTheAction>
+          <h1>Participe da nossa comunidade no discord. <br />É rápido e indolor.</h1>
+          <h2>Todos os integrantes são muito simpáticos. Manda um salve no chat-geral.</h2>
+          <a href="https://discord.gg/AHbF8BK" target="_blank" rel="noreferrer">Acessar!</a>
+          <img src="./images/community.svg" alt="Pessoas conversando digitalmente"/>
+        </CallTheAction>
+      </main>
     </>
   )
 }
